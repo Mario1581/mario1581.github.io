cd1eb8944469c864e50fd7d06ec9d9910f857b55
Mario1581 <mario-150895@hotmail.com>
2017-02-16 13:46:12 -0600

    Update Ejemplo2.9.4.js

--- Ejemplo2.9.4.js
+++ Ejemplo2.9.4.js
@@ -1,5 +1,5 @@
 var miCanvas = document.getElementByID("miCanvas");
-var miCanvas = document.getElementByID("miCanvas");
+var contexto =miCanvas.getContext("2d");
 
 contexto.fillStyle="red";
 contexto.fillRect(30,30,50,50);
