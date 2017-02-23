3638b9bbae4a08b9b957982819dfe39fa114c4a1
Mario1581 <mario-150895@hotmail.com>
2017-02-23 14:10:16 -0600

    Create ejemplo-rev.js

---
+++ ejemplo-rev.js
@@ -0,0 +1,26 @@
+var puntos = [];
+
+
+for ( var i = 0; i < 100; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.0281 ) * 50,
+                     i*0.973 );
+}
+
+var forma = new THREE.LatheGeometry(puntos);
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
